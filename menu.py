10c4e21ca8274e27dc023472decc874a369b4606
tekdodo <164498899+tekdodo@users.noreply.github.com>
2025-02-28 10:43:37 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,17 +1,4 @@
-# COMO FAZER UM MENU DE OPÇÕES
 
-print("1 - Opção 1")
-print("2 - Opção 2")
-print("3 - Opção 3")
-opcao = int(input("Digite a opção desejada:"))
-if (opcao == 1):
-    print("Você escolheu 1")
-elif (opcao == 2):
-    print("Você escolheu 2")
-elif (opcao == 3):
-    print("Você escolheu 3")
-else:
-    print("Opção inválida")
 
 import random
 
