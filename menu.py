a4173e769ebd508bacd2e7caaf2974e56b5399fb
tekdodo <164498899+tekdodo@users.noreply.github.com>
2025-02-28 10:23:02 -0300

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,68 @@
+# COMO FAZER UM MENU DE OPÇÕES
+
+print("1 - Opção 1")
+print("2 - Opção 2")
+print("3 - Opção 3")
+opcao = int(input("Digite a opção desejada:"))
+if (opcao == 1):
+    print("Você escolheu 1")
+elif (opcao == 2):
+    print("Você escolheu 2")
+elif (opcao == 3):
+    print("Você escolheu 3")
+else:
+    print("Opção inválida")
+
+import random
+
+# Exibindo o menu de dificuldades
+print("### MENU DE DIFICULDADE ###")
+print("1 - Fácil (1 a 30, 5 tentativas)")
+print("2 - Médio (1 a 80, 4 tentativas)")
+print("3 - Difícil (1 a 100, 3 tentativas)")
+
+# O usuário escolhe a dificuldade
+opcao = int(input("Digite a opção desejada: "))
+
+# Ajustando as configurações com base na escolha
+if opcao == 1:
+    numero_max = 30
+    tentativas_max = 5
+    print("Você escolheu a dificuldade Fácil!")
+elif opcao == 2:
+    numero_max = 80
+    tentativas_max = 4
+    print("Você escolheu a dificuldade Média!")
+elif opcao == 3:
+    numero_max = 100
+    tentativas_max = 3
+    print("Você escolheu a dificuldade Difícil!")
+else:
+    print("Opção inválida!")
+    exit()  # Sai do programa caso a opção seja inválida
+
+# Gerando o número sorteado
+sorteio = random.randint(1, numero_max)
+
+# Iniciando o jogo de adivinhação
+print(f"\nAdivinhe o número que estou pensando, de 1 a {numero_max}")
+tentativas = 1
+
+# Loop do jogo
+while tentativas <= tentativas_max:
+    print(f"Tentativa {tentativas}/{tentativas_max}")
+    chute = int(input("Digite o seu chute: "))
+
+    if chute == sorteio:
+        print(f"Parabéns, você acertou! O número era {sorteio}.")
+        break
+    elif chute > sorteio:
+        print("Chute um número menor.")
+    else:
+        print("Chute um número maior.")
+
+    tentativas += 1
+
+if chute != sorteio:
+    print(f"Você não acertou. O número era {sorteio}.")
+
